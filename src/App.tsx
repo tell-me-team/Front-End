e4d401407b64145ac493c94f4dbc40160b4a82b8
jiin9999 <cji3604@gmail.com>
2023-07-27 00:52:05 +0900

    Test: [#noissue] API 테스트를 위한 버튼 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [status, setStatus] = useState(false);
+
+  const testApi = async () => {
+    try {
+      const response = await axios.get("https://tell-me.store/healthcheck");
+      if (response.status === 200) {
+        setStatus(true);
+      } else {
+        setStatus(false);
+      }
+    } catch (error) {
+      console.error("API test failed:", error);
+    }
+  };
+
   return (
     <div>
-      <p>기본설정</p>
+      <button onClick={testApi}>API 테스트</button>
+      <p>↓ 성공 여부 /기본 값은 실패이며, 성공시 성공으로 변경/</p>
+      {status ? <p>성공</p> : <p>실패</p>}
     </div>
   );
 }
