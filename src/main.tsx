a4670dba3abe188c2bf015743811d3a588209b98
jiin9999 <cji3604@gmail.com>
2023-07-29 13:55:36 +0900

    feat: 로그인 기능 구현을 위한 라우팅 라이브러리 추가
    - react-router 라이브러리 추가
    - 라우팅 확인을 위한 임시 페이지 PageA 생성

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+
+import App from "./App";
+import PageA from "./pages/PageA";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="/pageA" element={<PageA />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ src/pages/PageA.tsx
@@ -0,0 +1,5 @@
+function PageA() {
+  return <div>페이지 A입니다.</div>;
+}
+
+export default PageA;
