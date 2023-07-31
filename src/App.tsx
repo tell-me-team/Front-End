cdbd11a4bde3a494ca0d4494d39ba362051bb42d
savazyggg <younji0715@gmail.com>
2023-07-31 11:27:56 +0900

    design: layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrPage from "./pages/ErrPage";
 import SurveyPage from "./components/surveyPage/SurveyPage";
 import TestMain from "./pages/testMain";
-
+import { Reset } from "styled-reset";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Layout from "./Layout";
@@ -13,18 +13,19 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element:  <Layout />,
+      element: <Layout />,
       errorElement: <ErrPage />,
       children: [
         { path: "/", element: <MainPage /> },
         { path: "/survey", element: <SurveyPage /> },
-        { path: "/auth/kakao", element: <KakaoLoginPage />},
+        { path: "/auth/kakao", element: <KakaoLoginPage /> },
       ],
     },
   ]);
-  
+
   return (
     <>
+      <Reset />
       <RouterProvider router={router} />
     </>
   );
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
+import { styled } from "styled-components";
 
 const Layout = () => {
   return (
-    <>
+    <SLayOut>
       <Outlet />
-    </>
+    </SLayOut>
   );
 };
 
 export default Layout;
+
+const SLayOut = styled.main`
+  max-width: 375px;
+  margin: 0 auto;
+`;
