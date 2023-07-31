a29b7b0380d2122ff32ad2ee46431659f8d198c6
jiin9999 <cji3604@gmail.com>
2023-07-31 15:34:48 +0900

    Feat: [#9] 배경화면 적용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,25 @@
 import KakaoLoginButton from "../components/mainPage/KakaoLoginButton";
+import { styled } from "styled-components";
 
 function MainPage() {
   return (
-    <div>
+    <SLayout>
       <KakaoLoginButton />
-    </div>
+    </SLayout>
   );
 }
+
+const SLayout = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  padding: 24px 40px;
+  background-image: url(/background/background_image_1.svg);
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const GlassBox = styled.div`
+  width: 100%;
+`;
 
 export default MainPage;
