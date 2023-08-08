80a05019ee67a019e394d94b4494510a167630ab
jiin9999 <cji3604@gmail.com>
2023-08-08 10:03:06 +0900

    Feat: [#21] API 수정

--- src/pages/KakaoLoginPage.tsx
+++ src/pages/KakaoLoginPage.tsx
@@ -1,3 +1,4 @@
+import styled, { keyframes } from "styled-components";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { socialLogin } from "../api/socialLogin";
@@ -22,10 +23,33 @@ function KakaoLoginPage() {
   }, [navigate]);
 
   return (
-    <div>
-      <p>들렀다 가는 페이지입니다</p>
-    </div>
+    <SLayout>
+      <StyledSpinner />
+    </SLayout>
   );
 }
+
+const SLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: calc(100vh - 80px);
+`;
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const StyledSpinner = styled.div`
+  border: 16px solid #f3f3f3; // Light grey
+  border-top: 16px solid #3498db; // Blue
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: ${spin} 2s linear infinite;
+`;
 
 export default KakaoLoginPage;
